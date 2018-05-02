5b40bac4238ec536ed0f0d084449bfa42740cecc
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 14:49:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = oldID + 1000000000
 
 let ageIsValid = Number.isInteger(currentAge)
 
-let randomNumber = Math.floor(Math.random() * 20);
+let randomNumber = Math.floor(Math.random() * 21);
