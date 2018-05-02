35c376e15a272b903bf1ef68670c5e09017c50b1
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 14:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * 20;
 
-let randomInteger = Math.floor(randomNumber)
+let randomInteger = Math.floor(randomNumber);
